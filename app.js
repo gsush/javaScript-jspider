12653448d8e4d0d8703246f88a39754de450000a
Snehangshu_Ghosh <gsush>
2021-02-22 20:10:41 +0530

    variables in javascript and scope of them

---
+++ app.js
@@ -0,0 +1,17 @@
+// console.log('hello');
+// var $user_name = `Hello`;
+// console.log($user_name);
+// var user2 = `nece`;
+// var user3 = "he";
+// console.log(typeof user2);
+let str = "123";
+console.log(typeof str); // string is the type of str value.
+let num = Number(str); // this converst string into a number who are having a number within a string
+console.log (typeof num); // and this time it will be a number
+let str1 = "Hello";
+// This Number is a part of type conversion.
+// Primitive types are 1.String,2.number,3.boolean,4.undefined,5.Null,6.symbol,7.Bigint
+// object reference (non-primitive data types) Array,object,function.
+console.log(str1); // this time it will be string
+let num1 = Number(typeof str1); // the string will not convert to a number
+console.log(num1); // hence the value will be NAN(not a number) will come as error.
