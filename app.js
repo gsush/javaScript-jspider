f46258218e5d0e04e8ab507263b5a55ba2204e04
Snehangshu_Ghosh <gsush>
2021-02-25 10:53:22 +0530

    functions iief and declaraion of using

--- app.js
+++ app.js
@@ -1,17 +1,78 @@
-// console.log('hello');
-// var $user_name = `Hello`;
-// console.log($user_name);
-// var user2 = `nece`;
-// var user3 = "he";
-// console.log(typeof user2);
-let str = "123";
-console.log(typeof str); // string is the type of str value.
-let num = Number(str); // this converst string into a number who are having a number within a string
-console.log (typeof num); // and this time it will be a number
-let str1 = "Hello";
-// This Number is a part of type conversion.
-// Primitive types are 1.String,2.number,3.boolean,4.undefined,5.Null,6.symbol,7.Bigint
-// object reference (non-primitive data types) Array,object,function.
-console.log(str1); // this time it will be string
-let num1 = Number(typeof str1); // the string will not convert to a number
-console.log(num1); // hence the value will be NAN(not a number) will come as error.
+// var x = "sashi";
+// //console.log(window);
+// console.log(global.x);
+/*
+
+let p = 10;
+function sam(){
+    let p = 11;
+    console.log(p);    
+}
+sam();
+
+var s = 10;
+function sunil(params) {
+    var s = 12;
+    console.log(s);
+}
+sunil();
+console.log(s);// here scope is here
+
+var c = 10;
+*/
+// 25 feb 2021 //
+// let spider = function () {
+//     console.log("i am funcion");
+// };
+
+// spider();
+
+// //IIFE
+// (function (params) {
+//     console.log("i am iief funcion");
+// })();
+
+
+// var $ = 'jquer';
+// var $ = 'reac';
+// var $ = 'angular';
+
+// console.log($);//angular all are global scope
+// poluing global scope
+// iife examples
+// (function (params) {
+//     var $ = 'jquer';// local var
+//     console.log($);
+// })();
+
+// (function (params) {
+//     var $ = 'reac'; // local var
+//     console.log($);
+// })();
+
+// (function (params) {
+//     var $ = 'angular';
+//     console.log($);// local var
+// })();
+
+
+{
+    let $ = 'jquer'; 
+    console.log($); // anoher wa of expressing local scope
+}
+{
+    let $ = 'reac';
+    console.log($);
+}
+{
+    let $ = 'angular';
+    console.log($);
+}
+
+
+//funcion declaraion
+
+function jspider(params) {
+    console.log('i am naming funcion');
+}
+jspider();
---
+++ app.ts
@@ -0,0 +1,10 @@
+// var x = "sashi";
+// //console.log(window);
+// console.log(global.x);
+
+let p = 10;
+function sam(){
+    let p = 11;
+    console.log(p);    
+}
+sam()
