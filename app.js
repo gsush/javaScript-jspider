e1c8ff06a6797c2ea6e9e6b0423a6415da04ece4
Snehangshu_Ghosh <gsush>
2021-02-27 23:17:51 +0530

    revision and scope

--- app.js
+++ app.js
@@ -1,42 +1,14 @@
-// function users(name) {
-//     return name;
-// }
-// console.log(users("hello"));
+// let darshan = (function (){
+//     console.log('message displeaesd');
+// }());
 
-// //funcion wih parameer
-
-// function emploee(emp_id,emp_name,emp_sal,emp_desg) {
-//     return {emp_id,emp_name,emp_sal,emp_desg}
-// }
-
-// let emp1 = emploee("one","sam",12000,"devloper");
-// let emp2 = emploee("four","ross",15000,'scienis'); 
-// console.log(emp1,emp2);
-
-// function Users(name,age,salar,compan) {
-//     return {
-//         name,age,salar,compan
-//     };// removing reurn saemen will cause undefined
-// }
-// let user1 = Users("sam",20,20000,"dlf");
-// console.log(user1);
-
-
-// function Users() {
-//     return arguments; // return values wih emp parameters built in array like object only available in normal func.
-// } // not available in arrow funcion
-
-// let emp = Users("manu",20,"ghosh");
-// console.log(emp[2]);
-
-// function jspider(...rest){
-//     return rest
-// }
-
-// console.log(jspider("fer","jaca","hjof"));
-
-function courses(course,rainer, ...rest) {
-    return{course,rainer,rest};
+var name = "ram";
+function displa(params) {
+    //var name = "raja";
+    var age =12;
+    console.log(name);
+    console.log(age);
 }
-
-console.log(courses("java","c","cpp","gho","ghj"));
+displa();
+console.log(name);
+console.log(age);
