ea314111ff37926a3db72b82c351d1f334d0f9c1
Snehangshu_Ghosh <gsush>
2021-03-13 01:12:27 +0530

    objects iteration and dom webpage

--- app.js
+++ app.js
@@ -1,73 +1,113 @@
-// 10 march 2021
-// Array.prototype.reduce(callbackfn)
-// retrun the sum of the array elements
-// let num = [10, 20, 30, 40, 50, 60, 70, 400];
-// let total = num.reduce(function (accumulator, currentValue) {
-//     console.log(accumulator);
-//     console.log(currentValue);
-//     return accumulator + currentValue;
-// });
-// console.log(total);
+// objects in javascript
+//let object = {}; // object literals
 
-// // single output value using all the elements of the array
-// let reduceData = ["s", "n", "e", 'h', 'a', 'n', 'g', 's', 'h', 'u'];
-// let newResult = reduceData.reduce((accumulator, currentValue) => accumulator + currentValue);
-// console.log(newResult);
+// object.name = "manu";
+// object.age = 20;
+// object.company = "jspider";
 
-// let newarray = [1, 2, 3, 4, 5];
-// const reducer = (accumulator, currentValue) => accumulator + currentValue;
-// console.log(newarray.reduce(reducer));
+// let arr = [];// array literals
+// arr["name"] = "namu";
+// console.log(object);
 
-// // reverse a string and print it
-// // first this is convert the string into an array and then use the reverse method and join to make it string
-// // again
-// let arr = ["s", "n", "e", 'h', 'a', 'n', 'g', 's', 'h', 'u'];
-// let res = arr.reduce((accumulator, currentValue) => {
-//     let x = accumulator + currentValue;
-//     console.log(typeof x);
-//     let reverstIt = [...x].reverse().join("");
-//     return reverstIt;
-// });
+// Object way to create objects
 
-// console.log(res);// There is a problem with this method hence we use reduceRight method
-// let res2 = arr.reduceRight((accumulator, currentValue) => accumulator + currentValue);
-// console.log(res2);
+// let users = new Object();
+// users.name = "dinesh";
+// users.company = "qspider";
+// users.salary = 20000;
+// users.designation = "fullstack developer";
+// console.log(users);
 
-// // two dimentional array 
+// // function constructor way
+// let users = function (name, age, company, salary, designation) {
+//     this.name = name;
+//     this.age = age;
+//     this.company = company;
+//     this.salary = salary;
+//     this.designation = designation;
+// }
 
-let twodarr = [[1, 2], [2, 2], [3, 3], [4, 4], [5, 5]];
-// let twodarray = [[1, 2], ['sam', 'ram', 'game', 'rani'], [3, 3], [4, 4]];
-// //console.log(twodarr[0][1]);
+// // console.log(users);
+// // console.log(new users);// with new users is a custom constructor
 
-// let neew = twodarr[2].forEach(x => x); // for each method no
-// console.log(neew);
+// let nami = new users("main", 20, "qspider", 200000, "developer");
+// let sam = new users("sam", 24, "wopro", 299999, "devops");
+// console.log(nami);
+// console.log(sam);
 
-// return a single value array 
-let outpt = twodarr.reduce((accumulator, currentValue) => {
-    console.log(currentValue);
-    return accumulator.concat(currentValue);
-});
-console.log(outpt);
+// // new object(); // built in constructor
 
-// // for the sum of the result outpt
-// let resultd = outpt.reduce((accumulator, currentValue) => accumulator + currentValue);
-// console.log(resultd);
+// let user = { username: "manu", username: "sashi", password: "ras@123" };
+// let pass = { password: "pass@123" };
 
-// // multidimentional array
-// let arra = [[1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1], [[[[[[[[[[[[[[[[1, 1, 1]]]]]]]]]]]]]]]]];
-// // instead of using reduce method we can use the flat method
-// let result = arra.flat(Infinity);
-// console.log(result);
+// //let combineObject = user.concat(password);// not work in object only in case of array
+// //console.log(combineObject);
 
-// let output = [...result].reduce((accumulator, currentValue) => accumulator + currentValue);
-// console.log(output);
+// console.log(Array.isArray(user));
 
-// Array.prototype.flat() this is the method used instead of using reduce method
+// // Object.assign method
+// let combineObject = Object.assign(user, pass);
+// console.log(combineObject);
 
-// `//Array.prototype.find(callbackfn) method
-// let num = [10, 30, 50, 60, 100, 30, 20, 300, 400,];
-// let result = num.find(element => element > 60);// the next number will print
-// let result1 = num.findIndex(element => element > 60); // index of the next number will print
-// console.log(result);
-// console.log(result1);`
+// // loop over an object 
+// let user = {
+//     username: "andy",
+//     age: 25,
+//     company: "qspider",
+//     salary: 50000,
+//     designation: "developer"
+// }
+// // for in loop for object iteration
+// for (let i in user) {
+//     // console.log(i);// key in the objects
+//     // console.log(user[i]);// values for the respective keys
+//     console.log(`${i} => ${user[i]}`);
+// }
+
+// store multiple objects
+let employees = [{
+    emp_id: "typ1",
+    emp_photo: "https://images.unsplash.com/photo-1553984658-d17e19aa281a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8ZmFzaGlvbiUyMG1vZGVsfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    emp_name: "rani",
+    emp_salary: 20000,
+    emp_designation: "developer",
+    emp_city: "delhi",
+    emp_educaton: "B.Tech"
+}, {
+    emp_id: "typ1",
+    emp_photo: "https://images.unsplash.com/photo-1583167461820-d79d51f89d7a?ixid=MXwxMjA3fDB8MHxzZWFyY2h8M3x8ZmFzaGlvbiUyMG1vZGVsfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    emp_name: "rani",
+    emp_salary: 20000,
+    emp_designation: "developer",
+    emp_city: "delhi",
+    emp_educaton: "B.Tech"
+}, {
+    emp_id: "typ1",
+    emp_photo: "https://images.unsplash.com/photo-1563993297290-609c9406efcd?ixid=MXwxMjA3fDB8MHxzZWFyY2h8N3x8ZmFzaGlvbiUyMG1vZGVsfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    emp_name: "rani",
+    emp_salary: 20000,
+    emp_designation: "developer",
+    emp_city: "delhi",
+    emp_educaton: "B.Tech"
+}]
+
+let output = "";
+for (let emp of employees) {
+    output += `
+    <section>
+    <article>
+    <div>
+    <img src=${emp.emp_photo} alt = ${emp.emp_name}/>
+    <h2> employee_name : ${emp.emp_name}</h2>
+    <p> emp_id : ${emp.emp_id}</p>
+    <p> salary : ${emp.emp_salary}</p>
+    <p> designation : ${emp.emp_designation}</p>
+    <p> city : ${emp.emp_city}</p>
+    <p> education : ${emp.emp_educaton}</p>
+    </div>
+    </article>
+    </section>`
+}
+
+document.write(output);
 
