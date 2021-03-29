0ad9aadfdc9c6d56ed162d74479100929ab47736
Snehangshu_Ghosh <gsush>
2021-03-29 22:47:02 +0530

    promises, methods all,any,race,then,catch,clock,setTimeout

--- app.js
+++ app.js
@@ -1,69 +1,108 @@
-// // form validation
-// let form = document.getElementById("form");
-// let username = document.getElementById("username");
-// let password = document.getElementById("password");
-// let errormessage = document.getElementById("errorMessage");
+// // promises 29/03/2021 ****************holi *********
+// // Example one for promise
+// let promise = new Promise((resolve, reject) => {
+//     // console.log(resolve);
+//     // console.log(reject);
+//     let isAvailable = false; // true and false value will give the required output
+//     if (isAvailable) {
+//         resolve("yes i am available");
+//     } else {
+//         reject("sorry not available");
+//     }
+// })
 
-// //**********************form submit */
-// form.addEventListener("submit", e => {
-//     e.preventDefault();
-//     let message = [];
-//     if (username.value === "" || username.value === null) {
-//         message.push('username is required');
+// promise.then(function (data) {
+//     console.log(data);
+// })
+
+// promise.catch(function (error) {
+//     console.log(error);
+// })
+
+// Example two for promise 
+
+// let room = new Promise((resolve, reject) => {
+//     let cleaned = false;
+//     if (cleaned) {
+//         resolve("yes room is cleaned");
+//     } else {
+//         reject("do something to clean");
 //     }
-//     if (password.value === "" || password.value === null) {
-//         message.push('password is required');
-//     }
-//     if (password.value.length < 6) {
-//         message.push('password should be minimum 6 character');
-//     }
-//     if (message.length > 0) {
-//         errormessage.innerHTML = message.join(" ");
-//     }
-//     else {
-//         console.log("username" + username.value,
-//             "password" + password.value);
+// })
+// room.then(data => console.log(data)).catch(err => console.log(err));
+
+// single thereaded event loop object used for the time delay perupose - setTimeout Function.
+
+// setTimeout(function () {
+//     console.log("hello i am timeout function"); //time is specified in ms 
+// }, 2000);
+
+// let user = "snehangshu";
+// console.log(user);
+// window.clearInterval();
+
+// promises are used to call api and connecting to AJAX calls
+
+// let promise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve("connect to uber")
+//     }, 1201) // resolve part
+//     setTimeout(() => {
+//         reject("ubable to connect to internet");
+//     }, 1200) // reject part
+// })
+
+// promise.then(data => console.log(data)).catch(err => console.log(err));
+
+//Methods in promises
+// 1. all()
+// 2. any()
+// 3. race()
+
+// let promise1 = new Promise((resolve, reject) => {
+//     let isJavaScriptGood = false;
+//     if (isJavaScriptGood) {
+//         resolve("yes i like javas")
+//     } else {
+//         reject("i don't like javascrpt")
 //     }
 // });
 
-// // bubbling and capturing
-let countries = ["Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegowina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory", "Brunei Darussalam", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Christmas Island", "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo", "Congo, the Democratic Republic of the", "Cook Islands", "Costa Rica", "Cote d'Ivoire", "Croatia (Hrvatska)", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands (Malvinas)", "Faroe Islands", "Fiji", "Finland", "France", "France Metropolitan", "French Guiana", "French Polynesia", "French Southern Territories", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Heard and Mc Donald Islands", "Holy See (Vatican City State)", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran (Islamic Republic of)", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea, Democratic People's Republic of", "Korea, Republic of", "Kuwait", "Kyrgyzstan", "Lao, People's Democratic Republic", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libyan Arab Jamahiriya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia, The Former Yugoslav Republic of", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia, Federated States of", "Moldova, Republic of", "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcairn", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russian Federation", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Seychelles", "Sierra Leone", "Singapore", "Slovakia (Slovak Republic)", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Georgia and the South Sandwich Islands", "Spain", "Sri Lanka", "St. Helena", "St. Pierre and Miquelon", "Sudan", "Suriname", "Svalbard and Jan Mayen Islands", "Swaziland", "Sweden", "Switzerland", "Syrian Arab Republic", "Taiwan, Province of China", "Tajikistan", "Tanzania, United Republic of", "Thailand", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "United States Minor Outlying Islands", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Virgin Islands (British)", "Virgin Islands (U.S.)", "Wallis and Futuna Islands", "Western Sahara", "Yemen", "Yugoslavia", "Zambia", "Zimbabwe"];
-output = "";
-countries.forEach(country => {
-    output = `<option> value = ${country} > ${country}</option>`;
-});
-document.getElementById('ul').innerHTML = output;// not working properly check it 
+// let promise2 = new Promise((resolve, reject) => {
+//     let isJavaScriptGood = false;
+//     if (isJavaScriptGood) {
+//         resolve("yes i like javast")
+//     } else {
+//         reject("i don't like javascrpt")
+//     }
+// });
 
-// capturing and bubbling allows us to implement one of the most powerful event pattern called event 
-// delgation.
+// let promise3 = new Promise((resolve, reject) => {
+//     let isJavaScriptGood = false;
+//     if (isJavaScriptGood) {
+//         resolve("yes i like javascript")
+//     } else {
+//         reject("i don't like javascrpt")
+//     }
+// });
 
-// let grandparent = document.getElementById('grandParent');
-// let parent = document.getElementById("parent");
-// let child = document.getElementById("child");
+// // Promise.all([promise1, promise2, promise3])
+// //     .then(data => console.log(data))
+// //     .catch(err => console.log(err));
 
-// grandparent.addEventListener('click', e => {
-//     console.log("grandparent clicked ....");
-// }, true)
 
-// parent.addEventListener('click', e => {
-//     console.log("parent clicked >>>>>>");
-// }, true)
 
-// child.addEventListener('click', e => {
-//     console.log("child ckicked");
-//     e.stopPropagation();
-// }, true)
+// Promise.any([promise1, promise2, promise3])
+//     .then(data => console.log(data))  // when all are false
+//     .catch(err => console.log(err)); //AggregateError: No Promise in Promise.any was resolved
 
-// // event deligation 
-// let ul = document.getElementById('ul');
-// ul.addEventListener('click', e => {
-//     console.log(e.target);
-// })
-
-// removing dom elements 
-// let ul = document.getElementById("ul");
-// ul.addEventListener("click", e => {
-//     console.log(e.target);
-//     let span = e.target.children;
-//     console.log(span);
-// })
+let sashi = new Promise((resolve, reject) => {
+    setTimeout(() => { resolve("sashi won") }, 2000)
+})
+let mani = new Promise((resolve, reject) => {
+    setTimeout(() => { resolve("main won") }, 2000)
+})
+let dinesh = new Promise((resolve, reject) => {
+    setTimeout(() => { resolve("dinesh won") }, 3000)
+})
+Promise.race([sashi, mani, dinesh]).then(data => console.log(data)).catch(err => console.log(err));
